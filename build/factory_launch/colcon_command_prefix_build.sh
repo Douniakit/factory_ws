4942a08a303166f9d57535b00ba5941d6add8528
dunish <kitounii.dounia@gmail.com>
2025-09-05 10:40:40 +0200

    removed unnecessary files
    remove the install/ build/ and log/ accidentally pushed

--- build/factory_launch/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/dounia/factory_ws/install/factory_robot_description/share/factory_robot_description/package.sh"
-. "/home/dounia/factory_ws/install/linear_axis_description/share/linear_axis_description/package.sh"
